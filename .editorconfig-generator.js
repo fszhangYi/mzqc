d5bc82c2ffc1f2fbdd9f54f5954fac5faaac1291
muzhongqing <daqi_supcon@163.com>
2023-11-05 21:08:59 +0800

    add one artical

--- .editorconfig-generator.js
+++ .editorconfig-generator.js
@@ -1,45 +1,12 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
 
-const editorConfig = `# 全局配置
-root = true
-
-# 通用配置
-[*]
-indent_style = space
-indent_size = 2
-end_of_line = lf
-charset = utf-8
-trim_trailing_whitespace = true
-insert_final_newline = true
-
-# JavaScript 配置
-[*.js]
-indent_size = 4
-
-# Python 配置
-[*.py]
-indent_size = 4`;
-
+// editorconfig
+const editorConfig = fs.readFileSync(path.join(__dirname, './template/.editorConfig'));
 fs.writeFileSync('.editorconfig', editorConfig);
 
-const gitignore = `# dependencies
-/node_modules
-/npm-debug.log*
-/yarn-error.log
-package-lock.json
-
-# production
-/dist
-
-# misc
-.DS_Store
-
-# umi
-/src/.umi
-/src/.umi-production
-/src/.umi-test
-/.env.local`;
-
+// gitignore
+const gitignore = fs.readFileSync(path.join(__dirname, './template/.gitignore'));
 fs.writeFileSync('.gitignore', gitignore);
