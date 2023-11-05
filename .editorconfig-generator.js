6dcef00857a801f82053600ccdee3891179a0dd1
muzhongqing <daqi_supcon@163.com>
2023-11-05 21:23:16 +0800

    add one artical

--- .editorconfig-generator.js
+++ .editorconfig-generator.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const path = require('path');
 
 // editorconfig
-const editorConfig = fs.readFileSync(path.join(__dirname, './template/.editorConfig'));
+const editorConfig = fs.readFileSync(path.join(__dirname, './template/editorConfig'));
 fs.writeFileSync('.editorconfig', editorConfig);
 
 // gitignore
-const gitignore = fs.readFileSync(path.join(__dirname, './template/.gitignore'));
+const gitignore = fs.readFileSync(path.join(__dirname, './template/gitignore'));
 fs.writeFileSync('.gitignore', gitignore);
